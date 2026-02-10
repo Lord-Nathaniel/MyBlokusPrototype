da9b0b6d2c4dca88b55076450ade4496828ded8e
Adrien Lozano <ad_lozano@outlook.com>
2026-02-11 00:01:10 +0100

    feat(MenuGameZone): WIP added player one input
    Color select is done
    Texture select is ongoing
    Name input is to do

--- Assets/Scripts/MenuManager.cs
+++ Assets/Scripts/MenuManager.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -6,15 +7,27 @@ public class MenuManager : MonoBehaviour
     [Header("Buttons Settings")]
     [SerializeField] private Button gameButton;
     [SerializeField] private Button optionsButton;
+    [SerializeField] private Button creditsButton;
     [SerializeField] private Button quitButton;
 
     [Header("Game Settings")]
     [SerializeField] private GameObject gameZone;
     [SerializeField] private Button gameStartButton;
 
+    [Header("Player One Settings")]
+    [SerializeField] private Button playerOneColorButton;
+    [SerializeField] private Button playerOneTextureButton;
+    [SerializeField] private InputField playerOneInputField;
+    [SerializeField] private GameObject playerOneColorSelectZone;
+    [SerializeField] private GameObject playerOneTextureSelectZone;
+
     [Header("Options Settings")]
     [SerializeField] private GameObject optionsZone;
     [SerializeField] private Button optionsCloseButton;
+
+    private Color playerOneColor;
+    private List<Color> playerColors = new();
+    private List<Texture2D> playerTextures = new();
 
 
     private void Awake()
@@ -29,6 +42,15 @@ public class MenuManager : MonoBehaviour
 
     private void Start()
     {
+        playerColors.Add(new Color(0.033f, 0, 1f));
+        playerColors.Add(new Color(1f, 0, 0));
+        playerColors.Add(new Color(1f, 1f, 0));
+        playerColors.Add(new Color(0.5f, 0, 0.5f));
+        playerColors.Add(new Color(0.2f, 0.8f, 0.2f));
+        playerColors.Add(new Color(1f, 0.633f, 0));
+        playerColors.Add(new Color(0, 1f, 1f));
+        playerColors.Add(new Color(1f, 0.42f, 0.71f));
+
         gameButton.onClick.AddListener(() =>
         {
             GameAction();
@@ -53,24 +75,42 @@ public class MenuManager : MonoBehaviour
         {
             Application.Quit();
         });
+
+        playerOneColorButton.onClick.AddListener(() =>
+        {
+            Toggle(playerOneColorSelectZone);
+        });
+
+        playerOneTextureButton.onClick.AddListener(() =>
+        {
+            Toggle(playerOneTextureSelectZone);
+        });
     }
 
     private void OptionAction()
     {
         Hide(gameZone);
-        if (optionsZone.activeInHierarchy)
-            Hide(optionsZone);
-        else
-            Show(optionsZone);
+        Toggle(optionsZone);
     }
 
     private void GameAction()
     {
         Hide(optionsZone);
-        if (gameZone.activeInHierarchy)
-            Hide(gameZone);
-        else
-            Show(gameZone);
+        Toggle(gameZone);
+    }
+
+    public void SelectPlayerOneColor(int colorID)
+    {
+        Color color = playerColors[colorID];
+        playerOneColor = color;
+        playerOneColorButton.image.color = color;
+        Hide(playerOneColorSelectZone);
+    }
+
+    public void SelectPlayerOneTexture(int textureID)
+    {
+        Texture2D texture = playerTextures[textureID];
+        Hide(playerOneTextureSelectZone);
     }
 
     private void Show(GameObject toShow)
@@ -81,5 +121,13 @@ public class MenuManager : MonoBehaviour
     private void Hide(GameObject toHide)
     {
         toHide.SetActive(false);
+    }
+
+    private void Toggle(GameObject toToggle)
+    {
+        if (toToggle.activeInHierarchy)
+            toToggle.SetActive(false);
+        else
+            toToggle.SetActive(true);
     }
 }
