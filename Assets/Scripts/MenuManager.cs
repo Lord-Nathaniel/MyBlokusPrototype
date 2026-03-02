bdcba1e0bc3aea69c07a41e3237623f558bb65fc
Adrien Lozano <ad_lozano@outlook.com>
2026-03-02 23:47:50 +0100

    fix(MenuScene): refactor players setting up in the menu

--- Assets/Scripts/MenuManager.cs
+++ Assets/Scripts/MenuManager.cs
@@ -6,6 +6,11 @@ using UnityEngine.Localization.Settings;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
+/// <summary>
+/// This class manages the keyboard and mouse input.
+/// When the player interact with the game, it calls corresponding Actions.
+/// -IN- PlayerPieceManager | PreviewManager
+/// </summary>
 public class MenuManager : MonoBehaviour
 {
     [Header("Buttons Settings")]
@@ -20,37 +25,18 @@ public class MenuManager : MonoBehaviour
     [SerializeField] private Material playerColorSwap;
     [SerializeField] private Toggle textureToggle;
 
-    [Header("Player One Settings")]
-    [SerializeField] private Button playerOneColorButton;
-    [SerializeField] private Button playerOneTextureButton;
-    [SerializeField] private TMP_InputField playerOneInputField;
-    [SerializeField] private GameObject playerOneColorSelectZone;
-    [SerializeField] private GameObject playerOneTextureSelectZone;
+    [Header("Player Settings")]
+    [SerializeField] private List<Button> playerColorButtons;
+    [SerializeField] private List<Button> playerTextureButtons;
+    [SerializeField] private List<TMP_InputField> playerInputFields;
+    [SerializeField] private List<GameObject> playerColorSelectZones;
+    [SerializeField] private List<GameObject> playerTextureSelectZones;
 
-    [Header("Player Two Settings")]
-    [SerializeField] private Button playerTwoColorButton;
-    [SerializeField] private Button playerTwoTextureButton;
-    [SerializeField] private TMP_InputField playerTwoInputField;
-    [SerializeField] private GameObject playerTwoColorSelectZone;
-    [SerializeField] private GameObject playerTwoTextureSelectZone;
-
-    [Header("Player Three Settings")]
+    [Header("Player Three and Four Settings")]
     [SerializeField] private Toggle playerThreeToggle;
     [SerializeField] private GameObject playerThreeZone;
-    [SerializeField] private Button playerThreeColorButton;
-    [SerializeField] private Button playerThreeTextureButton;
-    [SerializeField] private TMP_InputField playerThreeInputField;
-    [SerializeField] private GameObject playerThreeColorSelectZone;
-    [SerializeField] private GameObject playerThreeTextureSelectZone;
-
-    [Header("Player Four Settings")]
     [SerializeField] private Toggle playerFourToggle;
     [SerializeField] private GameObject playerFourZone;
-    [SerializeField] private Button playerFourColorButton;
-    [SerializeField] private Button playerFourTextureButton;
-    [SerializeField] private TMP_InputField playerFourInputField;
-    [SerializeField] private GameObject playerFourColorSelectZone;
-    [SerializeField] private GameObject playerFourTextureSelectZone;
 
     [Header("Selectable Textures")]
     [SerializeField] private List<Texture2D> cellTextures;
@@ -68,16 +54,10 @@ public class MenuManager : MonoBehaviour
     [SerializeField] private GameObject creditsZone;
     [SerializeField] private Button creditsCloseButton;
 
+    private int maxPlayerNb = 4;
+
     private List<Color> playerColors = new();
-    private List<Texture2D> playerTextures = new();
-    private Color playerOneColor;
-    private Color playerTwoColor;
-    private Color playerThreeColor;
-    private Color playerFourColor;
-    private int playerOneTextureId;
-    private int playerTwoTextureId;
-    private int playerThreeTextureId;
-    private int playerFourTextureId;
+    private List<int> playerTextureIds = new();
     private int currentLanguageIndex;
 
     const string GAME_SCENE = "GameScene";
@@ -106,10 +86,12 @@ public class MenuManager : MonoBehaviour
         playerColors.Add(new Color(0.2f, 0.8f, 0.2f));
         playerColors.Add(new Color(0.24f, 0.79f, 0.88f));
         playerColors.Add(new Color(1f, 0.42f, 0.71f));
-        playerColors.Add(new Color(.6f, 0.6f, 0.6f));
+        playerColors.Add(new Color(0.6f, 0.6f, 0.6f));
 
-        SelectPlayerOneColor(1);
-        SelectPlayerOneTexture(0);
+        for (int i = 0; i < maxPlayerNb; i++)
+        {
+            SetPlayerMenu(i);
+        }
 
         gameButton.onClick.AddListener(() =>
         {
@@ -146,36 +128,53 @@ public class MenuManager : MonoBehaviour
             Application.Quit();
         });
 
-        playerOneColorButton.onClick.AddListener(() =>
-        {
-            Toggle(playerOneColorSelectZone);
-        });
-
-        playerOneTextureButton.onClick.AddListener(() =>
-        {
-            Toggle(playerOneTextureSelectZone);
-        });
-
         languageButton.onClick.AddListener(() =>
         {
             ToggleLanguage();
         });
     }
 
+    private void SetPlayerMenu(int i)
+    {
+        int index = i;
+        SelectPlayerColor(index, index);
+        SelectPlayerTexture(index, 0);
+
+        playerColorButtons[index].onClick.AddListener(() =>
+        {
+            Toggle(playerColorSelectZones[index]);
+        });
+
+        playerTextureButtons[index].onClick.AddListener(() =>
+        {
+            Toggle(playerTextureSelectZones[index]);
+        });
+
+        for (int j = 0; j < playerColors.Count; j++)
+        {
+            playerColorSelectZones[index].transform.GetChild(j).GetComponent<Button>().onClick.AddListener(() =>
+            {
+                SelectPlayerColor(index, j);
+            });
+        }
+
+        for (int k = 0; k < cellTextures.Count; k++)
+        {
+            playerTextureSelectZones[index].transform.GetChild(k).GetComponent<Button>().onClick.AddListener(() =>
+            {
+                SelectPlayerTexture(index, k);
+            });
+        }
+    }
+
     private void StartGameAction()
     {
-        //TODO complete with true values
-        string playerOneName = playerOneInputField.text;
-        string playerTwoName = playerTwoInputField.text;
-        string playerThreeName = playerThreeInputField.text;
-        string playerFourName = playerFourInputField.text;
-
-        playerSetup.AddPlayerSetting(0, playerOneName, playerOneColor, playerOneTextureId);
-        playerSetup.AddPlayerSetting(1, playerTwoName, playerTwoColor, playerTwoTextureId);
+        playerSetup.AddPlayerSetting(0, playerInputFields[0].text, playerColors[0], playerTextureIds[0]);
+        playerSetup.AddPlayerSetting(1, playerInputFields[1].text, playerColors[1], playerTextureIds[1]);
         if (playerThreeToggle.isOn)
-            playerSetup.AddPlayerSetting(2, playerThreeName, playerThreeColor, playerThreeTextureId);
+            playerSetup.AddPlayerSetting(2, playerInputFields[2].text, playerColors[2], playerTextureIds[2]);
         if (playerFourToggle.isOn)
-            playerSetup.AddPlayerSetting(3, playerFourName, playerFourColor, playerFourTextureId);
+            playerSetup.AddPlayerSetting(1, playerInputFields[3].text, playerColors[3], playerTextureIds[3]);
 
         playerSetup.AddOptionsSetting(soundSlider.GetComponent<Slider>().value,
                                       musicSlider.GetComponent<Slider>().value,
@@ -184,6 +183,9 @@ public class MenuManager : MonoBehaviour
         SceneManager.LoadScene(GAME_SCENE);
     }
 
+    /// <summary>
+    /// Toggle to the next available language.
+    /// </summary>
     public void ToggleLanguage()
     {
         List<Locale> locales = LocalizationSettings.AvailableLocales.Locales;
@@ -220,80 +222,89 @@ public class MenuManager : MonoBehaviour
         Toggle(gameZone);
     }
 
-    public void SelectPlayerOneColor(int colorID)
+    private void SelectPlayerColor(int playerId, int colorID)
     {
-        Debug.Log("Selected colorID: " + colorID);
         Color color = playerColors[colorID];
-        playerOneColor = color;
+        playerColors[playerId] = color;
 
-        Graphic playerOneColorButtonGraphic = playerOneColorButton.GetComponent<Graphic>();
+        Graphic playerOneColorButtonGraphic = playerColorButtons[playerId].GetComponent<Graphic>();
         Material baseMat = playerOneColorButtonGraphic.materialForRendering;
         Material playerOneColorButtonMaterial = new Material(baseMat);
-        playerOneColorButtonMaterial.color = playerOneColor;
+        playerOneColorButtonMaterial.color = playerColors[playerId];
         playerOneColorButtonGraphic.material = playerOneColorButtonMaterial;
 
-        Graphic playerOneTextureButtonGraphic = playerOneTextureButton.GetComponent<Graphic>();
+        Graphic playerOneTextureButtonGraphic = playerTextureButtons[playerId].GetComponent<Graphic>();
 
         Material runtimeMaterialInstance = new Material(playerColorSwap);
         runtimeMaterialInstance.SetColor("_PlayerColor", color);
         playerOneTextureButtonGraphic.material = runtimeMaterialInstance;
 
-        for (int i = 0; i < playerOneTextureSelectZone.transform.childCount; i++)
+        for (int i = 0; i < playerTextureSelectZones[playerId].transform.childCount; i++)
         {
-            Graphic graphic = playerOneTextureSelectZone.transform.GetChild(i).gameObject.GetComponent<Graphic>();
+            Graphic graphic = playerTextureSelectZones[playerId].transform.GetChild(i).gameObject.GetComponent<Graphic>();
 
             Material runtimeMaterialInstanceTex = new Material(playerColorSwap);
             runtimeMaterialInstanceTex.SetColor("_PlayerColor", color);
             graphic.material = runtimeMaterialInstanceTex;
         }
-        Hide(playerOneColorSelectZone);
+        Hide(playerColorSelectZones[playerId]);
     }
 
-    public void SelectPlayerOneTexture(int textureID)
+    private void SelectPlayerTexture(int playerId, int textureID)
     {
-        playerOneTextureId = textureID;
+        playerTextureIds.Add(textureID);
         Texture2D selectedTexture = cellTextures[textureID];
 
-        Graphic playerOneTextureButtonGraphic = playerOneTextureButton.GetComponent<Graphic>();
+        Graphic playerOneTextureButtonGraphic = playerTextureButtons[playerId].GetComponent<Graphic>();
 
         Material runtimeMaterialInstance = new Material(playerColorSwap);
-        runtimeMaterialInstance.SetColor("_PlayerColor", playerOneColor);
+        runtimeMaterialInstance.SetColor("_PlayerColor", playerColors[playerId]);
         runtimeMaterialInstance.SetTexture("_MainTex", selectedTexture);
         playerOneTextureButtonGraphic.material = runtimeMaterialInstance;
-        Hide(playerOneTextureSelectZone);
+        Hide(playerTextureSelectZones[playerId]);
     }
 
+    /// <summary>
+    /// Allow the player three to be selected.
+    /// </summary>
     public void TogglePlayerThree()
     {
         Toggle(playerThreeZone);
     }
 
+    /// <summary>
+    /// Allow the player four to be selected.
+    /// </summary>
     public void TogglePlayerFour()
     {
         Toggle(playerFourZone);
     }
 
+    /// <summary>
+    /// Allow the textures to be selected.
+    /// </summary>
     public void ToggleTextureButtons()
     {
-        Toggle(playerOneTextureButton.transform.parent.gameObject);
-        Toggle(playerTwoTextureButton.transform.parent.gameObject);
-        Toggle(playerThreeTextureButton.transform.parent.gameObject);
-        Toggle(playerFourTextureButton.transform.parent.gameObject);
+        for (int i = 0; i < playerTextureButtons.Count; i++)
+        {
+            Toggle(playerTextureButtons[i].transform.parent.gameObject);
+        }
     }
 
+    /// <summary>
+    /// Update the sound value text when the slider value changes.
+    /// </summary>
     public void UpdateSoundTextValue()
     {
         soundValueText.text = soundSlider.GetComponent<Slider>().value.ToString();
     }
 
+    /// <summary>
+    /// Update the music value text when the slider value changes.
+    /// </summary>
     public void UpdateMusicTextValue()
     {
         musicValueText.text = musicSlider.GetComponent<Slider>().value.ToString();
-    }
-
-    private void Show(GameObject toShow)
-    {
-        toShow.SetActive(true);
     }
 
     private void Hide(GameObject toHide)
